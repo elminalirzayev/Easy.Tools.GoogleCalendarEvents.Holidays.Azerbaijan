89f80dc2143e8f273a2ba1b47ff399fb841bf819
Elmin Alirzayev <elmin.alirzayev@gmail.com>
2025-07-31 12:10:11 +0300

    Add project files.

---
+++ Easy.Tools.GoogleCalendarEvents.Holidays.Azerbaijan/Program.cs
@@ -0,0 +1,16 @@
+﻿using Easy.Tools.GoogleCalendarEvents.Holidays.Azerbaijan.Enum;
+using Easy.Tools.GoogleCalendarEvents.Holidays.Azerbaijan.Service;
+using System;
+
+
+//var service = new HolidayService("YOUR_API_KEY");
+var service = new HolidayService("AIzaSyBbAJaPVtNCk9ioDk8IjOJgb2hIk6D3AYY");
+
+var holidays = await service.GetHolidaysAsync(CalendarLanguage.Turkish);
+
+foreach (var item in holidays)
+{
+    Console.WriteLine(item);
+}
+
+
